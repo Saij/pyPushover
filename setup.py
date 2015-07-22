cb2e387f1b364cf1c18c5232f9069a0b5d6b6e57
Saij <christoph@christophfriedrich.de>
2015-07-22 10:57:04 +0200

    o Initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = "pypushover",
+    version = "0.0.1",
+    url = "https://github.com/Saij/pyPushover",
+    author = "Christoph Friedrich",
+    scripts = ["pypushover"],
+    author_email = "christoph@christophfriedrich.de",
+    description = "Command-Line tool for Pushover API",
+    platforms = "any",
+)
